cd28fafb3a8ec9f8464edbfc3d6dcd280d4228ed
James Y <yuan.james@outlook.com>
2024-03-18 01:36:57 +0000

    .

--- CLONALG.py
+++ CLONALG.py
@@ -4,6 +4,7 @@ import pandas as pd
 import time
 import os
 #test!!!
+#print('yes, it works!')
 def clonal_selection(benchmark_number, pop_size, clone_factor, mutation_rate):
     start_time = time.time()
     globalMin, Lb, Ub, nd, max_limit = bh.terminate(benchmark_number)
---
+++ test.py
@@ -0,0 +1,1 @@
+print('yes, it works!')
